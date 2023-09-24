fcc5e99e214dba91106120502407b7a56982e1ea
William Andersson <william99.andersson@telia.com>
2023-09-24 20:48:37 +0200

    Bugfix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.stream.IntStream;
 
@@ -33,16 +35,15 @@ public class Main {
             cells.forEachRemaining(cell -> {
                 for (char number : cell.getStringCellValue().toCharArray()) {
                     i[1] = 0;
-                    Row row2 = sheetFinal.createRow(i[0]++);
-                    Arrays.asList(new Object[]{startDate[0].toString(), number, i[2]}).forEach(n -> row2.createCell(i[1]++).setCellValue(String.valueOf(n)));
-                    startDate[0] = startDate[0].plusDays(1);
                     i[2] = i[2] + Integer.parseInt(String.valueOf(number));
+                    Row row2 = sheetFinal.createRow(i[0]++);
+                    Arrays.asList(startDate[0].toString(), String.valueOf(number), String.valueOf(i[2])).forEach(n -> row2.createCell(i[1]++).setCellValue(n));
+                    startDate[0] = startDate[0].plusDays(1);
                 }
             });
-
-
         });
         IntStream.range(0, 3).forEach(sheetFinal::autoSizeColumn);
         workBookFinal.write(new FileOutputStream(desktopUrl + "final.xlsx"));
+
     }
 }
